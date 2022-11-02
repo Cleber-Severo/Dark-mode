e99828964f70eb10e35a48c6d62f944bcddec15b
Cléber Severo <clebersevero1998@gmail.com>
2022-11-02 10:30:31 -0300

    Dark mode cards background

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 const darkToggle = document.getElementById('darkToggle')
 const topContainer = document.querySelector('.top__container')
 
-console.log(darkToggle)
+//const x = document.querySelectorAll('.followers__card') 
+const cards = document.getElementsByTagName('article')
 
+var counter = 0
+
+function cardlight(){
+
+    while (counter <= 11) {
+        cards[counter].style.backgroundColor = 'var(--lightMode-cardBg-grayishBlue)'
+        counter++
+        }
+
+    counter = 0
+
+}
+
+function cardDark(){
+
+    while (counter <= 11) {
+         cards[counter].style.backgroundColor = 'var(--darkMode-Card-BG-blue)'
+        counter++
+        }
+
+    counter = 0;
+
+}
 
 
 darkToggle.addEventListener('click', ()=> {
@@ -10,9 +34,12 @@ darkToggle.addEventListener('click', ()=> {
     if(darkToggle.checked) {
         document.body.classList.add("dark");
         topContainer.style.backgroundColor = 'var(--lightMode-bgTop)'
+        cardlight()
+   
     } else {
         document.body.classList.remove("dark");
         topContainer.style.backgroundColor = 'var(--darkMode-bgTop)'
+        cardDark()
     }
 
 })
